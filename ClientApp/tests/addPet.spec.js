cde36f45d0423bdebc681e676e7a0ef7dadc2c25
Lauren Peppard <PEPPARDLL@innovasi.com>
2019-04-12 02:29:27 -0700

    Added a couple more unit tests.

--- ClientApp/tests/addPet.spec.js
+++ ClientApp/tests/addPet.spec.js
@@ -1,7 +1,7 @@
 import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import AddPet from '@/components/PetClinic/AddPet.vue'
 import { createLocalVue } from '@vue/test-utils'
-
 import BootstrapVue from 'bootstrap-vue'
 
 
@@ -12,11 +12,19 @@ const localVue = createLocalVue()
 localVue.use(BootstrapVue)
 
 describe('AddPet.vue', () => {
-    it('renders props.msg when passed', () => {
-        const msg = 'new message'
-        const wrapper = shallowMount(AddPet, {
-            localVue
-        })
+    const wrapper = mount(AddPet, {
+        localVue
+    })
+
+    it('Displays correct title', () => {
         expect(wrapper.text()).toMatch('Add New Pet')
+    })
+
+    it('has a button', () => {
+        expect(wrapper.contains('button')).toBe(true)
+    })
+
+    test('renders correctly', () => {
+        expect(wrapper.element).toMatchSnapshot()
     })
 })
